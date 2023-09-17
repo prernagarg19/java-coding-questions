0e711ce1cb130214a26a01d709d391aabb2e3ba1
Prerna Garg <prernagarg19101996@gmail.com>
2023-09-17 17:34:16 +0530

    Update TwoSumProblem.java

--- Arrays/TwoSumProblem.java
+++ Arrays/TwoSumProblem.java
@@ -11,6 +11,17 @@ Output: Because nums[0] + nums[1] == 9, we return [0, 1]
 
 Input: nums = [3,2,4], target = 6
 Output: [1,2]
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target. You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+Examples
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Output: Because nums[0] + nums[1] == 9, we return [0, 1]
+
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
 */
 class TwoSumProblem {
 	public static void main(String[] args) {
@@ -27,6 +38,8 @@ class TwoSumProblem {
 			for(int j=i+1; j<nums.length; j++) {
 				if(nums[i]+ nums[j] == target) {
 					return new int[] {i, j};
+                    // arr[0]= i;
+                    // arr[1]=j;
 				}
 			}
 		}
@@ -52,17 +65,9 @@ class TwoSumProblem {
 
 
 /*
- * class Solution {
-    public int[] twoSum(int[] nums, int target) {
-        int[] arr = new int[2];
-        for(int i=0; i<nums.length; i++) {
-            for(int j=i+1; j<nums.length; j++) {
-                if(nums[i]+ nums[j] == target) {
-                    arr[0]= i;
-                    arr[1]=j;
-                }
-            }
-        }
-        return arr;
-    }
-}*/
+ * COMPLEXITY
+ * This solution uses two nested loops to find the pair of numbers that add up to the target, 
+ * resulting in a time complexity of O(n^2), where n is the length of the input array. 
+ * It does not use any library or data structure. However, this solution is less efficient than the HashMap solution 
+ * with a time complexity of O(n). We have not covered HashMap yet that is why I have used this solution.
+ */
