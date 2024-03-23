88abaddca2d808510a2df420d4e10288fc8edd02
Prerna Garg <prernagarg19101996@gmail.com>
2024-03-23 23:51:45 +0530

    Insertion In Doubly Linked List

--- Linked List/Doubly Linked List/DoublyLinkedList.java
+++ Linked List/Doubly Linked List/DoublyLinkedList.java
@@ -14,4 +14,39 @@ public class DoublyLinkedList {
         tail = newNode;
         return head;
     }
+
+    /*Insertion in Doubly Linked List */
+    public void insertInDoublyLinkedList(int nodeValue, int location) {
+        DoublyNode newNode = new DoublyNode();
+        newNode.value = nodeValue;
+        if(head == null) {
+            createDLL(nodeValue);
+            return;
+        }else if(location == 0) {
+            newNode.prev = null;
+            newNode.next = head;
+            head.prev = newNode;
+            head = newNode;
+        } else if(location >= size) {
+            newNode.next = null;
+            tail.next = newNode;
+            newNode.prev = tail;
+            tail = newNode;
+        } else {
+            DoublyNode tempNode = head;
+            // for(int i=0; i<location-1; i++) {
+            //     tempNode = tempNode.next;
+            // }
+            int index = 0;
+            while(index < location-1) {
+                tempNode = tempNode.next;
+                index++;
+            }
+            newNode.prev = tempNode;
+            newNode.next = tempNode.next;
+            tempNode.next = newNode;
+            newNode.next.prev = newNode;
+        }
+        size++;
+    }
 }
--- Linked List/Doubly Linked List/MainDoublyLinkedList.java
+++ Linked List/Doubly Linked List/MainDoublyLinkedList.java
@@ -2,7 +2,11 @@ public class MainDoublyLinkedList {
    public static void main(String[] args) {
     DoublyLinkedList dLL = new DoublyLinkedList();
     dLL.createDLL(5);
+    dLL.insertInDoublyLinkedList(2, 0);
+    dLL.insertInDoublyLinkedList(3, 1);
+    dLL.insertInDoublyLinkedList(4, 7);
     System.out.println(dLL.head.value);
+    System.out.println(dLL.head.next.value);
    }
     
 }
