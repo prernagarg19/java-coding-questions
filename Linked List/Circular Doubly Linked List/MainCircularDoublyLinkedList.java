a2f414c9ada71ed0dec98832194efecbfb65befd
Prerna Garg <prernagarg19101996@gmail.com>
2024-03-31 01:29:18 +0530

    Update MainCircularDoublyLinkedList.java

--- Linked List/Circular Doubly Linked List/MainCircularDoublyLinkedList.java
+++ Linked List/Circular Doubly Linked List/MainCircularDoublyLinkedList.java
@@ -2,8 +2,9 @@ public class MainCircularDoublyLinkedList {
     public static void main(String[] args) {
         CircularDoublyLinkedList cDLL = new CircularDoublyLinkedList();
         cDLL.createCDLL(1);
-        cDLL.insertNodeInCDLL(3,0);
-        cDLL.insertNodeInCDLL(2,1);
+        cDLL.insertNodeInCDLL(2, 0);
+        cDLL.insertNodeInCDLL(3, 2);
+        cDLL.insertNodeInCDLL(4, 5);
         cDLL.traverseCDLL();
         // cDLL.reverseTraversalCDLL();
         // cDLL.searchCDLL(3);
