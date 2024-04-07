f41f73e7c14b190481c07f7aa60dbcac3f6b1098
Prerna Garg <prernagarg19101996@gmail.com>
2024-04-07 23:04:32 +0530

    Reverse Rotation of Linked List

--- Linked List/LinkedList Questions/LinkedListMain.java
+++ Linked List/LinkedList Questions/LinkedListMain.java
@@ -4,15 +4,20 @@ public class LinkedListMain {
         ll.createLL(1);
         ll.insertNodeInLL(2);
         ll.insertNodeInLL(3);
-        ll.insertNodeInLL(1);
         ll.insertNodeInLL(4);
+        ll.insertNodeInLL(5);
         ll.insertNodeInLL(2);
+        ll.insertNodeInLL(1);
         ll.traverseLL();
         RemoveDuplicates deleteDups = new RemoveDuplicates();
         deleteDups.removeDuplicates(ll);
         ll.traverseLL();
         RotateLinkedList lList = new RotateLinkedList();
         lList.rotateLinkedList(ll, 2);
+        System.out.println("After rotation : ");
+        ll.traverseLL();
+        lList.reverseRotateLinkedList(ll,2);
+        System.out.println("After Reverse rotation : ");
         ll.traverseLL();
     }
 }
--- Linked List/LinkedList Questions/RotateLinkedList.java
+++ Linked List/LinkedList Questions/RotateLinkedList.java
@@ -1,5 +1,5 @@
 /*
- * This function should rotate all the nodes in the list by some number passed in. For instance, if your list looks like 1 -> 2 -> 3 -> 4 -> 5 and you rotate by 2, the list should be modified to 3 -> 4 -> 5 -> 1 -> 2. The number passed in  to rotate can be any integer.
+ * This function should rotate all the nodes in the list by some number passed in.
     Time Complexity : O(N), where N is the length of the list. Space Complexity : O(1)
  */
 
@@ -8,6 +8,7 @@ public class RotateLinkedList {
     public Node tail;
     public int size;
     
+    /* 1 -> 2 -> 3 -> 4 -> 5 and you rotate by 2 then it should be  4-> 5-> 1 -> 2 -> 3 */
     public Node rotateLinkedList(LinkedList lList,int number) {
         int length = 1;
         Node tempNode = lList.head;
@@ -16,14 +17,14 @@ public class RotateLinkedList {
             length++;
         }
         if(number%length == 0)  {
-            return head;
+            return lList.head;
         }
         number = number%length;
         tempNode.next = lList.head;
         Node newLastNode = findNthNode(lList.head, (length-number));
         lList.head = newLastNode.next;
         newLastNode.next = null;
-        return head;
+        return lList.head;
     }
 
     public Node findNthNode(Node head, int n) {
@@ -37,5 +38,23 @@ public class RotateLinkedList {
             current = current.next; 
         } 
         return null;
+    }
+
+    /*  if your list looks like 1 -> 2 -> 3 -> 4 -> 5 and you rotate by 2, the list should be modified to 3 -> 4 -> 5 -> 1 -> 2 */
+    public Node reverseRotateLinkedList(LinkedList lList, int number) {
+        Node tempNode = lList.head;
+        int length = 1;
+        while (tempNode.next != null) {
+            tempNode = tempNode.next;
+            length++;
+        }
+        if(number%length == 0)
+            return lList.head;
+        number = number%length;
+        tempNode.next = lList.head;
+        Node newFirstNode = findNthNode(lList.head, (length-number));
+        lList.tail = newFirstNode.prev;
+        lList.head = newFirstNode;
+        return lList.head;
     }
 }
